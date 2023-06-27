228a30a12e942388ad7fc7bf54dcde969ad52b10
Ashutosh-Mishra2 <ashutoshsitu99@gmail.com>
2023-06-27 08:31:37 +0000

    fidelity for optimizing all pulses

--- c3/libraries/fidelities.py
+++ c3/libraries/fidelities.py
@@ -1238,3 +1238,30 @@ def qubit_reset(states: tf.Tensor, index, dims, params, n_eval=-1):
 
     infid = 1 - max_overlap
     return tf.abs(infid)
+
+
+@fid_reg_deco
+def readout_clear_swap_prod(states: tf.Tensor, index, dims, params, n_eval=-1):
+    print("Calculating fidelity")
+    a_rotated = params["a_rotated"]
+    d_max = params["cutoff_distance"]
+    lindbladian = params["lindbladian"]
+    swap_target_g = params["swap_target_ground"]
+    swap_target_e = params["swap_target_excited"]
+
+    psis_g = states[0]
+    psis_e = states[1]
+
+    alphas_g = calculate_expect_value(psis_g, a_rotated, lindbladian)
+    alphas_e = calculate_expect_value(psis_e, a_rotated, lindbladian)
+
+    distances = tf.abs(alphas_g - alphas_e)
+    max_distance = tf.reduce_max(distances)
+    readout_fid = 1 - tf.exp(-max_distance / d_max)
+
+    overlap_g = calculate_state_overlap(psis_g[-1], swap_target_g)
+    overlap_e = calculate_state_overlap(psis_e[-1], swap_target_e)
+
+    swap_fid = (overlap_e + overlap_g) / 2
+
+    return 1 - (readout_fid * swap_fid)
